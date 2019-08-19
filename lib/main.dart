f3fb358111ad53c9102c11b3244f6694e6400397
Vladimir Zdravkovic <widrans@gmail.com>
2019-08-19 21:59:56 +0200

    added crashlytics to android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_xo/screens/login_screen.dart';
 import 'package:flutter_xo/screens/main_screen.dart';
 import 'package:flutter_xo/screens/splash_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
